62a40b1791baa91cee3da3ad3350f738a959145a
tutorial6 <manapure.vaibhav542@gmail.com>
2022-04-27 01:26:00 +0530

    Inserting data to table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -10,3 +10,11 @@ Name varchar(20),
 Salary varchar(10),
 StartDate varchar(20)
 )
+/* UC3 create employee payroll data */
+insert into employee_payroll(Name, Salary, StartDate) values ('Vaibhav','40000','16/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Ankush','50000','21/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Satish','60000','17/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Riya','35000','21/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Priya','25000','16/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Bhanu','35000','24/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Harish','25000','29/08/2021')
